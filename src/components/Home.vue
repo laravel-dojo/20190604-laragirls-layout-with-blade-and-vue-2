d066fc20b601e4c2fcf1046bbe6875592bc52845
Shengyou Fan <shengyoufan@gmail.com>
2019-06-02 23:47:32 +0800

    call api

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -83,6 +83,11 @@
                 items: []
             }
         },
+        created() {
+            this.axios.get('http://127.0.0.1:8000/api/items').then(response => {
+                this.items = response.data
+            })
+        }
     }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
+import VueAxios from 'vue-axios'
+import axios from 'axios'
 import App from './App.vue'
 
+Vue.use(VueAxios, axios);
 Vue.config.productionTip = false
 
 new Vue({
