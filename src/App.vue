8b181240011c85972a1ed2e2ddd9525a7ab76cf8
Shengyou Fan <shengyoufan@gmail.com>
2019-06-02 23:30:13 +0800

    basic structure

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Navigation/>
+    <Home/>
+    <Footer/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navigation from './components/Navigation.vue'
+import Footer from './components/Footer.vue'
+import Home from './components/Home.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Navigation,
+    Footer,
+    Home
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="col-lg-3">
+
+        <h1 class="my-4">Shop Name</h1>
+        <div class="list-group">
+            <a href="#" class="list-group-item">Category 1</a>
+            <a href="#" class="list-group-item">Category 2</a>
+            <a href="#" class="list-group-item">Category 3</a>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Sidebar.vue"
+    }
+</script>
+
+<style scoped>
+
+</style>
